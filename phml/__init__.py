9fe6e932a84f2bd5c31b8d0dacd1c13c555d30e3
Zachary Boehm <zboehm@menard-inc.com>
2022-11-10 12:25:13 -0600

    Spellcheck overview

--- phml/__init__.py
+++ phml/__init__.py
@@ -7,17 +7,17 @@ inject javascript data into html using brackets. A lot of the frameworks also
 have custom attributes on elements for conditional rendering.
 
 phml strives to do similar things, However, the nuance here
-is that phml uses python. There is a new html element that is resurved for phml.
+is that phml uses python. There is a new html element that is reserved for phml.
 This element is the `<python>` element. It means to replicate the purpose of the
 `<script>` element. On top of this there are smaller blocks that use brackets
 that expect the expression to return some sort of value. Finally, there are custom
 attributes. `py-if`, `py-elif`, and `py-else` are reserved for conditional rendering.
-All attributes prefixed with `py-` will have their value evalutated as python. More details
+All attributes prefixed with `py-` will have their value evaluated as python. More details
 on each of these topics is described below.
 
 Let's start with the new `python` element. Python is a whitespace language. As such phml
-has the chanllenge of maintaining the indentation in a appropriate way. With this in phml,
-you can have as much leading whitespace as you want as long as the indentation is consistant.
+has the challenge of maintaining the indentation in a appropriate way. With this in phml,
+you can have as much leading whitespace as you want as long as the indentation is consistent.
 This means that indentation is based of the first lines offset. Take this phml example:
 
 ```python
@@ -37,7 +37,7 @@ if True:
 So now we can write python code, now what? You can define functions and variables
 how you normally would and they are now available to the scope of the entire file.
 Take, for instance, the example from above, the one with `py-src="urls('youtube')"`.
-You can define the urls function in the `python` element and it can be accessed in the `py-src`
+You can define the URLs function in the `python` element and it can be accessed in the `py-src`
 attribute. So the code would look like this:
 
 ```html
@@ -58,13 +58,13 @@ def urls(link: str) -> str:
 ```
 
 phml treats `python` elements as their own python files. This is meant literally.
-phml creates a temporarty python file with a unique name and imports everything from that file.
+phml creates a temporary python file with a unique name and imports everything from that file.
 With that in mind that means you have the full power of whatever version of python you are using.
-The phml parser has functionality to perminantly write these python files out if you so desire.
+The phml parser has functionality to permanently write these python files out if you so desire.
 
 Next up is inline python blocks. These are represented with `{}`. Any text with this block or
-normal html attribute with the value containing it will process the text inbetween the brackets as
-python. This is mostly usefull when you want to inject some python processed or stored value
+normal html attribute with the value containing it will process the text in-between the brackets as
+python. This is mostly useful when you want to inject some python processed or stored value
 into html. Assume that there is a variable defined in the `python` element called `message`
 and it contains `Hello World!`. Now this variable can be used like this, `<p>{ message }</p>`,
 which renders to, `<p>Hello World!</p>`. Inline python blocks are only rendered in inside a Text
@@ -72,16 +72,16 @@ element or inside an html attribute.
 
 Conditional Rendering with `py-if`, `py-elif`, and `py-else` is an extremely helpful tool in phml.
 `py-if` can be used alone and that the python inside it's value must be truthy for the element to be
-rendered. `py-elif` requires an element with a `py-if` or `py-elif` attribute immediatly before it,
+rendered. `py-elif` requires an element with a `py-if` or `py-elif` attribute immediately before it,
 and it's condition is rendered the same as `py-if` but only rendered if a `py-if` or `py-elif` first
-fails. `py-else` requires there to be either a `py-if` or a `py-else` immediatly before it. It only
+fails. `py-else` requires there to be either a `py-if` or a `py-else` immediately before it. It only
 renders if the previous elements condition fails. If `py-elif` or `py-else` is on an element, but
 the previous element isn't a `py-if` or `py-elif` then it will be rendered as normal without the
 condition. Most importantly, the first element in a chain of conditions must be a `py-if`.
 
 Python attributes are shortcuts for using inline python blocks in html attributes. Normally, in
 phml, you would inject python logic into an attribute similar to this: `src="{url('youtube')}"`.
-As a way to simplify the value and to make it obvious that the attribute is using pyhon you can
+As a way to simplify the value and to make it obvious that the attribute is using python you can
 prefix any attribute with `py-`. This keeps the attribute name the same after the prefix, but tells
 the parser that the entire value should be processed as python. So the previous example can also be
 expressed as `py-src="url('youtube')"`.
