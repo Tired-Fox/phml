6f6a17b4a778dce5df33605436b745aeb14c0d1d
Zachary Boehm <zboehm@menard-inc.com>
2022-11-11 15:38:44 -0600

    Add desc for utils docs

--- phml/utils/__init__.py
+++ phml/utils/__init__.py
@@ -1,3 +1,9 @@
+"""phml.Utils
+
+This is a collection of utility functions that allow for manipulation, traversal,
+and discovery inside a phml.ast.AST.
+"""
+
 from . import find, transform, travel, misc, validate
 
 __all__ = [
