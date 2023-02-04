0b9a713643a9a57b0c10a2fea29c0cf5e5b8626f
Zachary Boehm <zboehm104@gmail.com>
2023-02-04 16:25:23 -0600

    Fix needing to use global KW to expose to method,class

--- phml/core/virtual_python/vp.py
+++ phml/core/virtual_python/vp.py
@@ -45,12 +45,47 @@ class VirtualPython:
                     self.imports.append(Import.from_node(node))
 
             # Retreive locals from content
-            local_env = {}
-            global_env = {**self.context}
-            exec(self.content, global_env, local_env)  # pylint: disable=exec-used
-            
-            local_env.update({key:value for key,value in global_env.items() if key not in globals() and key not in self.context})
-            self.context.update(local_env)
+            # local_env = {}
+            # global_env = {**self.context}
+            # exec(self.content, global_env, local_env)  # pylint: disable=exec-used
+
+            # local_env.update({key:value for key,value in global_env.items() if key not in globals() and key not in self.context})
+            # self.context.update(local_env)
+            self.context = self.get_python_context(self.content)
+
+    def get_python_context(self, source: str) -> dict[str, Any]:
+        """Get the locals built from the python source code string.
+        Splits the def's and classes into their own chunks and passes in
+        all other local context to allow for outer scope to be seen in inner scope.
+        """
+
+        chunks = [[]]
+        lines = source.split("\n")
+        i = 0
+
+        # Split the python source code into chunks
+        while i < len(lines):
+            if lines[i].startswith(("def","class")):
+                chunks.append([lines[i]])
+                i+=1
+                while i < len(lines) and lines[i].startswith((" ", "\t")):
+                    chunks[-1].append(lines[i])
+                    i+=1
+                chunks.append([])
+                continue
+
+            chunks[-1].append(lines[i])
+            i+=1
+
+        chunks = ["\n".join(chunk) for chunk in chunks]
+        local_env = {}
+
+        # Process each chunk and build locals
+        for chunk in chunks:
+            exec(chunk, {**local_env}, local_env)
+
+        return local_env
+
 
     def __add__(self, obj: VirtualPython) -> VirtualPython:
         local_env = {**self.context}
