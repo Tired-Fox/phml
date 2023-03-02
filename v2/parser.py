334e1deac7e3d998f0dc8d072188258094254cb2
Zachary Boehm <zboehm@menard-inc.com>
2023-03-02 17:15:15 -0600

    Parse attributes in v2

--- v2/parser.py
+++ v2/parser.py
@@ -30,7 +30,7 @@ REGEX = {
         r"<!--(?P<comment>[\w\W]*)-->|<(?P<name>[\w]+(?:[\w.:]+)*)(?P<attrs>(?:\s*([\w:.]+=\"[^\"]*\"|[\w:.]+='[^']*'|[\w:.]+=[^ />]+|[\w:.]+))*)\s*(?P<closing>\/)?>|<(?!!--)(?=\s*/?>)"
     ),
     "end_tag": "</\\s*{}\\s*>",
-    "attr": re.compile(r"((?P<n1>[\w:.]+)='(?P<v2>[^']*)'|(?P<n2>[\w:.]+)=\"(?P<v2>[^\"]*)\"|(?P<n3>[\w:.]+)=(?P<v3>[^>\s]+)|(?P<n4>[\w:.]+))"),
+    "attr": re.compile(r"((?P<n1>[\w:.]+)='(?P<v1>[^']*)'|(?P<n2>[\w:.]+)=\"(?P<v2>[^\"]*)\"|(?P<n3>[\w:.]+)=(?P<v3>[^>\s]+)|(?P<n4>[\w:.]+))"),
 }
 
 RElem = dict[Literal["name", "attrs", "comment", "closing"], str|None]
@@ -38,9 +38,62 @@ RElem = dict[Literal["name", "attrs", "comment", "closing"], str|None]
 Element = dict[Literal["_type", "tag", "attrs", "children"], Any]
 Comment = dict[Literal["_type", "name", "data"], str]
 
+def parse_attr_value(attrs: str, stop_chars: str|None=None):
+    stop_pattern = (
+            re.compile(f"^[{stop_chars}]$")
+            if stop_chars is not None
+            else re.compile(r"^[>\s]$")
+    )
+    i = 0
+    while i < len(attrs) and stop_pattern.match(attrs[i]) is None:
+        input("attr value: " + attrs[i])
+        i += 1
+        print(stop_pattern.match(attrs[i]), attrs[i]) if i < len(attrs) else print("OOB")
+
+    if stop_chars is not None and attrs[i] not in stop_chars:
+        raise ValueError("Attribute not closed. Expected <{', '.join(repr(c) for c in stop_chars)}>")
+
+    return attrs[:i], i
+
 def parse_attrs(attrs: str) -> dict | None:
-    for attr in REGEX["attr"].finditer(attrs):
-        print(attr)
+    attrs = attrs.strip()
+    i = 0
+    result = {}
+    attr_name = ""
+    while i < len(attrs): 
+        input(attrs[i])
+        if attrs[i] == "=":
+            input(attrs[i+1])
+            i += 1
+            if attrs[i] in ['"', "'"]:
+                i+=1
+                value, x = parse_attr_value(attrs[i:], attrs[i-1])
+                i += x
+                result[attr_name] = value
+                attr_name = ""
+            elif attrs[i] not in [" ", "\n", "\t"]:
+                value, x = parse_attr_value(attrs[i:])
+                i += x
+                result[attr_name] = value
+                attr_name = ""
+            else:
+                raise ValueError("Expected attribute value after '='")
+        elif attrs[i] in [" ", "\n", "\t"]:
+            if attr_name != "":
+                result[attr_name] = True
+                attr_name = ""
+            i += 1
+            while attrs[i] in [" ", "\n", "\t"]:
+                i += 1
+            continue
+        else:
+            attr_name += attrs[i]
+        i+=1
+
+    if attr_name != "":
+        result[attr_name] = attrs[i:] if i < len(attrs) else True
+    input(result)
+    return None
 
 def parse_tags(data: str, context: Element):
     while REGEX["element"].search(data) is not None:
