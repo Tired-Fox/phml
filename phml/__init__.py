cb2007531a1e94bf426b6f5a11c0fe74f1e3262b
Zachary Boehm <zboehm@menard-inc.com>
2023-03-17 17:02:18 -0500

    Create version object and manual test functionality

--- phml/__init__.py
+++ phml/__init__.py
@@ -1,5 +1,30 @@
 """
 .. include:: ../README.md
 """
+from dataclasses import dataclass
 from .core import PHML, Formats
-__version__ = "0.2.3"
+@dataclass
+class Version:
+    """Version object for phml.
+    
+    {Major}.{Minor}.{Alpha}
+
+    Alpha:
+        Includes all bugfixes and small feature changes that go into the
+        iterations of a task.
+
+    Minor:
+        All alpha changes pulled together into a task version release.
+
+    Major:
+        All minor changes pulled together into a collection of tasks into a
+        milestone/goal release.
+    """
+    Major: int = 0
+    Minor: int = 2
+    Alpha: int = 3
+
+    def __str__(self) -> str:
+        return f"{self.Major}.{self.Beta}.{self.Alpha}"
+
+__version__ = str(Version)
